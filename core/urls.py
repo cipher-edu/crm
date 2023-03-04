591560a119ecaa6d3d3f3978f3047fdba9f4bf6e
cipher-edu <ciphereduuz@gmail.com>
2023-03-04 10:47:14 +0500

    add

--- core/urls.py
+++ core/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', home, name='home'),
+    path('mijoz-yaratish', addclients, name='addclient'),
+    path('maxsuot-yaratish', additems, name="additems"),
+    path('tashkilot-qo\'shish', addorganizations, name="addorganizations"),
+    path('ish-yaratish', addworker, name="addworker"),
+    path('mijozlar', clients, name="clients")
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,33 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def addclients(request):
+    return render(request, 'pages/addcliens.html')
+
+def additems(request):
+    return render(request, 'pages/aditems.html')
+
+
+def addorganizations(request):
+    return render(request, 'pages/addorganizations.html')
+
+def addworker(request):
+    return render(request, 'pages/addworker.html')
+
+
+def clients(request):
+    return render(request, 'pages/clients.html')
+
+
+def clientservice(request):
+    return render(request, 'pages/clientservice.html')
+
+def endcerviceliend(request):
+    return render(request, 'pages/endcerviceliend.html')
+
+def organizations(request):
+    return render(request, 'pages/organizations.html')
+
+def workers(request):
+    return render(request, 'pages/worker.hml')
